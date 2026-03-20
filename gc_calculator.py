c4ee37ece375d45a5f67ad876d261992fb51886b
StevenChristian <stevenchristian@med.unr.edu>
2026-03-20 13:20:42 -0700

    Added the fixes for part b and corrected the script for part c

--- gc_calculator.py
+++ gc_calculator.py
@@ -17,15 +17,16 @@ def parse_fasta(filename):
         for line in f:
             line = line.strip()
             if line.startswith('>'):
-                header = line[1:]
+                header = line[1:].split()[0]
                 sequences[header] = ''
             else:
-                sequences[header] = line
+                sequences[header] += line
 
     return sequences
 
 def calculate_gc(sequence):
     """Calculate GC content as a percentage"""
+    sequence = sequence.upper()
     gc_count = sequence.count('G') + sequence.count('C')
     gc_content = gc_count / len(sequence) * 100
     return gc_content
@@ -34,7 +35,7 @@ def filter_by_length(sequences, min_length):
     """Filter sequences shorter than min_length"""
     filtered = {}
     for header, seq in sequences.items():
-        if len(seq) > min_length:
+        if len(seq) >= min_length:
             filtered[header] = seq
     return filtered
 
@@ -44,7 +45,7 @@ def main():
         sys.exit(1)
 
     input_file = sys.argv[1]
-    min_length = sys.argv[2]
+    min_length = int(sys.argv[2])
 
     sequences = parse_fasta(input_file)
     filtered = filter_by_length(sequences, min_length)
@@ -56,4 +57,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-```
